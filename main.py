b17c19fc001d11709bb3657c5d07dae0c3aa1d26
david.malkhasyan <malkhasyandavid@gmail.com>
2022-07-19 08:04:35 -0700

    POM structure creation

--- main.py
+++
@@ -1,7 +0,0 @@
-import  pytest
-
-def testMethod1():
-    print("This is the first method")
-
-def testMethod1():
-    print("This is the first method")
--- testCases/login_test.py
+++ testCases/login_test.py
@@ -2,11 +2,17 @@ from selenium import webdriver
 from pageObjects.LoginPage import Login
 
 class Test_Login_001:
-
+    baseURL = 'https://dev.squaresigns.com/'
 
 
 
     def test_homepage_title(self, setup):
         self.driver = setup
-        self.lp = Login(self.driver)
-        self.lp.open_my_account_menu()
+        self.driver.get(self.baseURL)
+        title = self.driver.title
+        self.driver.close()
+        if title == "Make your sign online | Customize design and print with Square Signs":
+            assert True
+        else:
+            assert False
+
